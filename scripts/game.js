f11b8b6ab2633d3016f3345446e7a2bdad1b1c75
Reniel George Mababa <mabsrencode@gmail.com>
2023-08-14 22:30:51 +0800

    Add files via upload

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,18 @@
-function resetGameStatus(){
+function resetGameStatus() {
   activePlayer = 0;
   currentRound = 1;
   gameIsOver = false;
-  gameOverElement.firstElementChild.innerHTML = 'You won!, <span id"winner-name">PLAYER NAME</span>!';
-  gameOverElement.style.display = 'none';
+  gameOverElement.firstElementChild.innerHTML =
+    'You won!, <span id"winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = "none";
 
   let gameBoardIndex = 0;
-  for(let i = 0; i < 3; i++){
-    for(let j = 0; j < 3; j++){
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
       gameData[i][j] = 0;
       const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
       gameBoardItemElement.textContent = "";
-      gameBoardItemElement.classList.remove('disabled');
+      gameBoardItemElement.classList.remove("disabled");
       gameBoardIndex++;
     }
   }
@@ -63,14 +64,14 @@ function selectGameField(event) {
   selectedField.classList.add("disabled");
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  
+
   const winnerId = checkForGameOver();
 
-  if(winnerId !== 0){
+  if (winnerId !== 0) {
     endGame(winnerId);
   }
-  
-currentRound++;
+
+  currentRound++;
   switchPlayer();
 }
 
@@ -108,20 +109,24 @@ function checkForGameOver() {
   ) {
     return gameData[2][0];
   }
-  if(currentRound === 9){
+  if (currentRound === 9) {
     return -1;
   }
   return 0;
 }
 
-function endGame(winnerId){
-  gameOverElement.style.display = 'block';
-  if(winnerId > 0){
+function endGame(winnerId) {
+  gameOverElement.style.display = "block";
+  if (winnerId > 0) {
     playerDisplay.style.display = "flex";
     const winnerName = players[winnerId - 1].name;
-    gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
-  }else{
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+    playerDisplay.style.display = "none";
+  } else {
     playerDisplay.style.display = "flex";
-    gameAreaElement.firstElementChild.firstElementChild.textContent = 'It\'s a draw!';
+    gameAreaElement.firstElementChild.firstElementChild.textContent =
+      "It's a draw!";
+    playerDisplay.style.display = "none";
   }
 }
